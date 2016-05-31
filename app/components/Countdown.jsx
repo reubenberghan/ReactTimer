823f7cfdc7cf637939bc736ad58c0bcdb6013ade
Reuben Berghan <reuben.berghan@gmail.com>
2016-06-01 06:30:53 +1000

    Add countdown functionality

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -7,12 +7,40 @@ var CountdownForm = require('CountdownForm');
 var Countdown = React.createClass({
     getInitialState: function getInitialStateCountdown() {
         return {
-            count: 0
+            count: 0,
+            countdownStatus: 'stopped'
         };
+    },
+    // `componentDidUpdate` is called every time the state of the component is changed
+    componentDidUpdate: function componentDidUpdateCountdown(prevProps, prevState) {
+        
+        // first we need to check our `countdownStatus` and if this has changed
+        // we use a `switch` statement to check the status and decide how to procede
+        if (this.state.countdownStatus !== prevState.countdownStatus) {
+            switch (this.state.countdownStatus) {
+                case 'started':
+                    this.startTimer();
+                    break;
+            }
+        }
+        
+    },
+    // our `startTimer` method will invoke `setInterval`, a built JavaScript method
+    // which will take one off the `count` and set the state at 1s intervals
+    startTimer: function startTimerCountdown() {
+        this.timer = setInterval(() => {
+            var newCount = this.state.count - 1;
+            this.setState({
+                // using the ternary so we don't count into the negatives
+                // the brackets aren't required but help readability of the expression
+                count: (newCount >= 0 ? newCount : 0)
+            });
+        }, 1000);
     },
     handleSetCountdown: function handleSetCountdown(seconds) {
         this.setState({
-            count: seconds
+            count: seconds,
+            countdownStatus: 'started'
         });
     },
     render: function renderCountdown() {
---
+++ app/tests/components/Countdown.test.jsx
@@ -0,0 +1,51 @@
+'use strict';
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
+
+var Countdown = require('Countdown');
+
+describe('Countdown', () => {
+    
+    it('should exist', () => {
+        expect(Countdown).toExist();
+    });
+    
+    describe('handleSetCountdown', () => {
+        
+        // by passing the `done` method to our tests we can make them asynchronous
+        it('should set state to started and count down', (done) => {
+            
+            var countdown = TestUtils.renderIntoDocument(<Countdown />);
+            countdown.handleSetCountdown(10);
+            
+            expect(countdown.state.count).toBe(10);
+            expect(countdown.state.countdownStatus).toBe('started');
+            
+            // we call the `done` method after our asynchronous assertion(s) to tell karma we are done
+            setTimeout(() => {
+                expect(countdown.state.count).toBe(9);
+                done();
+            }, 1001);
+            
+        });
+        
+        
+        it('should not count down below zero', (done) => {
+            
+            var countdown = TestUtils.renderIntoDocument(<Countdown />);
+            countdown.handleSetCountdown(1);
+            
+            setTimeout(() => {
+                expect(countdown.state.count).toBe(0);
+                done();
+            }, 3001);
+            
+        });
+        
+    });
+    
+});
