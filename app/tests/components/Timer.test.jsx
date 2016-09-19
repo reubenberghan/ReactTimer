d3547b87974f35cf085911ad7a7831df7dd9cb9d
Reuben Berghan <reuben.berghan@gmail.com>
2016-09-20 06:21:30 +1000

    Updated Timer tests

--- app/tests/components/Timer.test.jsx
+++ app/tests/components/Timer.test.jsx
@@ -34,28 +34,28 @@ describe('Timer', () => {
         it('should pause timer on paused status', (done) => {
 
             var timer = TestUtils.renderIntoDocument(<Timer />);
+
+            timer.setState({ count: 10 });
             timer.handleStatusChange('started');
+            timer.handleStatusChange('paused');
 
             setTimeout(() => {
-                expect(timer.state.count).toBe(1);
-                timer.handleStatusChange('paused');
+                expect(timer.state.count).toBe(10);
                 expect(timer.state.timerStatus).toBe('paused');
-            }, 1001);
-
-            setTimeout(() => {
-                expect(timer.state.count).toBe(1);
                 done();
-            }, 1000);
+            }, 1001);
 
         });
 
         it('should reset count on stopped status', (done) => {
 
             var timer = TestUtils.renderIntoDocument(<Timer />);
+
+            timer.setState({ count: 10 });
             timer.handleStatusChange('started');
+            timer.handleStatusChange('stopped');
 
             setTimeout(() => {
-                timer.handleStatusChange('stopped');
                 expect(timer.state.count).toBe(0);
                 expect(timer.state.timerStatus).toBe('stopped');
                 done();
